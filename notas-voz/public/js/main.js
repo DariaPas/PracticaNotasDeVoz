6f358e5b4e52245c72335ff73baf74f5640c8eba
Daria <dpaslavska001@ikasle.ehu.eus>
2023-11-24 19:57:31 +0100

    milestone 1

--- notas-voz/public/js/main.js
+++ notas-voz/public/js/main.js
@@ -35,7 +35,14 @@ class App {
       //this.recordLogic(stream)
       this.initRecord(stream);
       this.initAudio();
+      document.getElementById('uploadbtn').addEventListener('click', () => {
+        console.log("upload1");
+        this.initUpload();
+      }
+      );
     }
+
+  
     
     recordLogic( stream ) {
        const record_button = document.getElementById('recordbtn')
@@ -71,8 +78,8 @@ class App {
             this.stopAudio();
           }
         });
-    }
     
+    }
     loadBlob(audioBlob) {
        const audioURL = URL.createObjectURL(audioBlob)
        this.audio.src = audioURL
@@ -99,6 +106,7 @@ class App {
         this.audio= document.getElementById('audioPlayer');
         this.audio.src = audioUrl;
         console.log("estoy aqui2")
+        this.setState('inactive');
       };
 
       
@@ -110,6 +118,7 @@ class App {
           this.stopRecording();
         }
       });
+      
 
     }
     
@@ -127,17 +136,35 @@ class App {
           document.getElementById('playbtn').disabled=false;
         }
       }
-      else if(this.mediaRecorder.state=='inactive') {
-        if(this.mediaRecorder.mimeType!='' || this.audioChunks.length>0) {
+      else if(this.state[0]=='p') {
+        // document.getElementById('playbtn').textContent="Stop audio";
+        this.audio.currentTime=0;
+        document.getElementById('demo').textContent=parseInt(this.audio.duration);
+         this.audio.ontimeupdate=()=>{
+           document.getElementById('demo').textContent=parseInt(this.audio.duration-this.audio.currentTime);
+         }
+         this.audio.onended=()=> {
+           //document.getElementById('playbtn').textContent="Play audio";
+           this.setState('inactive');
+         }
+         //document.getElementById('playbtn').textContent="Play audio";
+         this.setState('inactive');
+       }
+       else if(this.mediaRecorder.state=='inactive') {
+        document.getElementById('playbtn').textContent="Play audio";
+        console.log(this.state);
+        console.log(this.mediaRecorder.state);
+        if(this.audioChunks.length>0) {
           document.getElementById('playbtn').disabled=false;
         }
       }
+    }
       /*else if(this.mediaRecorder.state=='playing') {
         document.getElementById('playbtn').textContent="Stop audio"
         this.setState(this.audio.ontimeupdate);
       }*/
-    }
-
+    
+    
     record(){
        
         this.mediaRecorder.start();
@@ -157,7 +184,7 @@ class App {
     
     playAudio(){
       this.audio.play();
-     // this.setState('playing');
+      this.setState('playing');
      document.getElementById('playbtn').textContent="Stop audio"
     }
     
@@ -166,10 +193,89 @@ class App {
       document.getElementById('playbtn').textContent="Play audio"
     }
 
-    upload(){
+
+    initUpload() {
+      
+      
+        fetch('api/list')
+        .then(response => response.json())
+        .then(data => {
+          console.log('Data from server:', data);
+
+          let filesContainer = document.getElementById('fileListContainer');
+          //filesContainer.innerHTML = '';
+
+                // Iterate over the files and create HTML elements for each file
+                data.files.forEach(file => {
+                  console.log(file);
+
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `<p>Filename: ${file.filename}</p>
+                                          <p>Date: ${new Date(file.date)}</p>
+                                          <button class="copyButton">Copy</button>
+                                          <button class="deleteButton">Delete</button>`;
+                    filesContainer.appendChild(listItem);
+
+                    // Add a click event listener to the 'copy' button
+                    const copyButton = listItem.querySelector('.copyButton');
+                    copyButton.addEventListener('click', () => {
+                      const link = `/play/${file.filename}`;
+                      navigator.clipboard.writeText(file.filename)
+                      .then(() => {
+                          console.log('Text copied to clipboard:', file.filename);
+                          alert('Filename copied to clipboard: ' + file.filename);
+                      })
+                      .catch(err => {
+                          console.error('Unable to copy text to clipboard:', err);
+                      });
+
+                    });
+
+
+                    const deleteButton = listItem.querySelector('.deleteButton');
+                    deleteButton.addEventListener('click', () => {
+                    // Llamar al endpoint para borrar el archivo
+                    const uuid = this.uuid; // Reemplaza con tu lógica para obtener el uuid
+                    const filename = file.filename;
+
+                    fetch(`/api/delete/${uuid}/${filename}`, {
+                      method: 'DELETE'
+                    })
+                    .then(response => response.json())
+                    .then(result => {
+                    console.log('Archivo borrado con éxito:', result);
+                    // Puedes actualizar la lista de archivos después de borrar
+                    })
+                  .catch(error => {
+                    console.error('Error al borrar el archivo:', error);
+                  });
+                });
+              });
+
+         
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+          
+       
+      
+      
+    }
+
+
+
+
+    upload() {
+      console.log("upload");
       this.setState({ uploading: true }); // estado actual: uploading
       const body = new FormData(); // Mediante FormData podremos subir el audio al servidor
       body.append("recording", this.blob); // en el atributo recording de formData guarda el audio para su posterior subida
+      if (!localStorage.getItem("uuid")) { // si no está almacenado en localStorage
+        localStorage.setItem("uuid", uuidv4()); // genera y gaurda el uuid
+      }
+      this.uuid = localStorage.getItem("uuid"); // logra el uuid desde localStorage
       fetch("/api/upload/" + this.uuid, {
         method: "POST", // usaremos el método POST para subir el audio body,
       })
@@ -195,6 +301,7 @@ class App {
 const myApp = new App();
 
 myApp.init();
+
 
 //document.getElementById('recordbtn').addEventListener('click', () =>myApp.record());
       
