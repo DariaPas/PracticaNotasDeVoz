8c94e8752788c87bf64ef336a362d41d2394a49a
Nayi101 <najera17.ortega@gmail.com>
2024-01-05 10:51:58 +0100

    login y logout

--- notas-voz/routes/index.js
+++ notas-voz/routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+/////
+var session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+
+// Use the session middleware
+router.use(session({
+  secret: 'clavesecretaparaexpresss',
+  saveUninitialized: true, // create session even if there is nothing stored
+  resave: true, // save session even if unmodified
+  cookie: { maxAge: 60 * 60 * 1000 },
+ // store: new MongoStore({ url: 'mongodb://127.0.0.1:27017/test-app'})
+}));
+
+
+router.get('/',(req,res) => {
+  if(req.session.email) {
+    return res.redirect('/admin');
+  }
+  res.render('index', { title : 'title'});
 });
+
+router.post('/login',(req,res) => {
+  req.session.email = req.body.email;
+  res.end('done');
+});
+
+router.get('/admin',(req,res) => {
+  if(req.session.email) {
+    res.write(`<h1>Hello ${req.session.email} </h1><br>`);
+    res.end('<a href='+'/logout'+'>Logout</a>');
+  }
+  else {
+    res.write('<h1>Please login first.</h1>');
+    res.end('<a href='+'/'+'>Login</a>');
+  }
+});
+
+router.get('/logout',(req,res) => {
+  req.session.destroy((err) => {
+    if(err) {
+      return console.log(err);
+    }
+    res.redirect('/');
+  });
+
+});/////
 
 module.exports = router;
